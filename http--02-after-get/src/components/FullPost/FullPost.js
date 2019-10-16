4b88001514d83b6e5f83333f71aa565d7b75735a
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-16 18:34:58 -0300

    INICIAL
    COMMIT INICIAL

--- http--02-after-get/src/components/FullPost/FullPost.js
+++ http--02-after-get/src/components/FullPost/FullPost.js
@@ -11,7 +11,7 @@ class FullPost extends Component {
     componentDidUpdate () {
         //não queremos fazer a solicitação com id nulo
 
-        
+
         if ( this.props.id ) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
                 axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.id )
@@ -23,6 +23,13 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler= () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/'+ this.props.id)
+        .then(response => {
+            console.log(response);
+        });
+    }
+
     render () {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if ( this.props.id ) {
@@ -32,9 +39,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
