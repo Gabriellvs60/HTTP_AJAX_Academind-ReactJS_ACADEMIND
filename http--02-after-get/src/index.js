67f12aedd4249ec70b94fd7fef333d6dda0294f5
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-16 19:10:50 -0300

    Interceptors
    Adicionando interceptors para executar o código de forma global
    
    *Assim podemos manipular ou criar logs na index.js do app React,
    
    GLOBAL

--- http--02-after-get/src/index.js
+++ http--02-after-get/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//bloqueiam requests se não der o retorno 
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
